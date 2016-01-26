0bbcb23134d059578f60dd4a29fe02e9515f0b43
ddavisgraphics <ddavisgraphics@gmail.com>
2016-01-26 09:59:17 -0500

    first commit

---
+++ SQLFiles/baseTables.sql
@@ -0,0 +1,10 @@
+-- Test Table
+-- -------------------------------------------------
+-- DROP TABLE IF EXISTS `test`;
+-- CREATE TABLE `test`(
+--     `ID` tinyint(3) unsigned NOT NULL AUTO_INCREMENT,
+--     `username` varchar(50) NOT NULL,
+--     `password` varchar(65) NOT NULL,
+--     PRIMARY KEY(`ID`)
+-- ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
---
+++ SQLFiles/setup.sql
@@ -0,0 +1,4 @@
+-- DROP DATABASE IF EXISTS `test`;
+-- CREATE DATABASE IF NOT EXISTS `test`;
+-- GRANT ALL PRIVILEGES ON `test`.* TO 'username'@'localhost';
+-- USE `test`;
